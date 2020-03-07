7470df4be3e96c7b510c6de95293f71dedcf4f51
Philip Lee-Chin <phil.leechin@outlook.com>
2020-03-07 17:02:03 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,13 +8,13 @@ class Anagram
   end
 
   def match
-    binding.pry
     new_arr = []
     splice_ph = phrase.split(' ')
     splice_ph.each do |ph|
       ph.sort
       new_arr << ph
     end
+    binding.pry
     new_arr
   end
 end
